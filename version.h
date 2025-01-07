7033f681b29c8d5b76a954ca990366e7c1749e03
Hotakus <ttowfive@gmail.com>
2025-01-07 18:03:09 +0800

    feat: 添加版本定义头文件及模板

---
+++ version.h
@@ -0,0 +1,13 @@
+#ifndef INCLUDE_GUARD
+#define INCLUDE_GUARD
+
+#define OCTO_FOC_NAME "octo-foc"
+#define OCTO_FOC_VER  "1.0.0.0"
+#define OCTO_FOC_VER_MAJOR "1"
+#define OCTO_FOC_VER_MINOR "0"
+#define OCTO_FOC_VER_PATCH "0"
+#define OCTO_FOC_VER_TWEAK "0"
+#define OCTO_FOC_DESC "Octo-FOC"
+#define OCTO_FOC_HOMEPAGE_URL ""
+
+#endif // INCLUDE_GUARD
