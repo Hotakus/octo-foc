fc71cc3f9cbbbd392662c45d9020fde7f0853790
Hotakus <ttowfive@gmail.com>
2025-02-05 12:57:51 +0800

    fix: 更新版本号和主页URL

--- version.h
+++ version.h
@@ -2,12 +2,12 @@
 #define INCLUDE_GUARD
 
 #define OCTO_FOC_NAME "octo-foc"
-#define OCTO_FOC_VER  "1.0.0.0"
+#define OCTO_FOC_VER  "1.0.2.0"
 #define OCTO_FOC_VER_MAJOR "1"
 #define OCTO_FOC_VER_MINOR "0"
-#define OCTO_FOC_VER_PATCH "0"
+#define OCTO_FOC_VER_PATCH "2"
 #define OCTO_FOC_VER_TWEAK "0"
 #define OCTO_FOC_DESC "Octo-FOC"
-#define OCTO_FOC_HOMEPAGE_URL ""
+#define OCTO_FOC_HOMEPAGE_URL "https://github.com/Hotakus/octo-foc"
 
 #endif // INCLUDE_GUARD
