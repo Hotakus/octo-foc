baea937abbac3843d65628931526b64fe11b83c5
Hotakus <ttowfive@gmail.com>
2025-02-09 16:47:08 +0800

    refactor: 更新octo-foc版本号至1.0.3.0

--- version.h
+++ version.h
@@ -2,10 +2,10 @@
 #define INCLUDE_GUARD
 
 #define OCTO_FOC_NAME "octo-foc"
-#define OCTO_FOC_VER  "1.0.2.0"
+#define OCTO_FOC_VER  "1.0.3.0"
 #define OCTO_FOC_VER_MAJOR "1"
 #define OCTO_FOC_VER_MINOR "0"
-#define OCTO_FOC_VER_PATCH "2"
+#define OCTO_FOC_VER_PATCH "3"
 #define OCTO_FOC_VER_TWEAK "0"
 #define OCTO_FOC_DESC "Octo-FOC"
 #define OCTO_FOC_HOMEPAGE_URL "https://github.com/Hotakus/octo-foc"
