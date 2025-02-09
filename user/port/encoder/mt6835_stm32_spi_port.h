696b8e44d179dcdc7a8a00a4712361d24e27b2e8
Hotakus <ttowfive@gmail.com>
2025-02-09 17:10:31 +0800

    feat(encoder): 添加MT6835 STM32 SPI端口驱动

---
+++ user/port/encoder/mt6835_stm32_spi_port.h
@@ -0,0 +1,26 @@
+/**
+  ******************************************************************************
+  * @file           : mt6835_stm32_spi_port.h
+  * @author         : Hotakus (hotakus@foxmail.com)
+  * @brief          : None
+  * @date           : 2025/2/9
+  ******************************************************************************
+  */
+
+#ifndef MT6835_STM32_SPI_PORT_H
+#define MT6835_STM32_SPI_PORT_H
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "mt6835.h"
+
+mt6835_t *mt6835_stm32_spi_port_init(void);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif  //MT6835_STM32_SPI_PORT_H
