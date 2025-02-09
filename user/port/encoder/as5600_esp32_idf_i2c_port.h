6cb78dddd4684b239fe4196956240e77821dfa56
Hotakus <ttowfive@gmail.com>
2025-02-09 17:51:12 +0800

    feat(encoder): 添加AS5600在ESP32-IDF上的I2C端口实现

---
+++ user/port/encoder/as5600_esp32_idf_i2c_port.h
@@ -0,0 +1,25 @@
+/**
+  ******************************************************************************
+  * @file           : as5600_esp32_idf_i2c_port.h
+  * @author         : Hotakus (hotakus@foxmail.com)
+  * @brief          : None
+  * @date           : 2025/2/9
+  ******************************************************************************
+  */
+
+#ifndef AS5600_ESP32_IDF_I2C_PORT_H
+#define AS5600_ESP32_IDF_I2C_PORT_H
+
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif  //AS5600_ESP32_IDF_I2C_PORT_H
