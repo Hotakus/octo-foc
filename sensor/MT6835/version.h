23d9e8ad55428ed6b2e609636614fc73b2f887ba
Hotakus <ttowfive@gmail.com>
2025-02-10 21:46:00 +0800

    feat(sensor): new sensor

---
+++ sensor/MT6835/version.h
@@ -0,0 +1,13 @@
+#ifndef INCLUDE_GUARD
+#define INCLUDE_GUARD
+
+#define OCTO_FOC_NAME "mt6835"
+#define OCTO_FOC_VER  "1.0.0.0"
+#define OCTO_FOC_VER_MAJOR "1"
+#define OCTO_FOC_VER_MINOR "0"
+#define OCTO_FOC_VER_PATCH "0"
+#define OCTO_FOC_VER_TWEAK "0"
+#define OCTO_FOC_DESC "mt6835-framework"
+#define OCTO_FOC_HOMEPAGE_URL "https://github.com/Hotakus/octo-foc"
+
+#endif // INCLUDE_GUARD
